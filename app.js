97c77b398abea7923d27ff149654be71c7973565
yildirimkadir <yildirimkad34@gmail.com>
2022-06-12 23:46:46 +0200

    Das Projekt wurde erstellt.

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+
+const screen = document.querySelector(".ekran");
+
+const containerDiv = document.querySelector(".container");
+
+// containerDiv.addEventListener("click", (event) => {
+    
+//     if (event.target.classList.contains("container")) {
+//         alert("Enter a number for an operation")
+//     } else if (event.target) {
+//         screen.innerHTML += `${event.target.innerHTML}`; 
+        
+//     } else if (screen.innerHTML.length == 6 ) {
+//         alert("6 oldu");
+//     }
+
+// }); 
+
+
+
